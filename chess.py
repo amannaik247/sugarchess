ab245f936874636f0c282ace95299f7f72cc5434
amannaik247 <amancodes247@gmail.com>
2025-01-02 14:05:46 +0530

    modified:   chess.py

--- chess.py
+++ chess.py
@@ -87,7 +87,7 @@ class Gnuchess():
 
         self._width = Gdk.Screen.width()
         self._height = Gdk.Screen.height()
-        self.scale = int((self._height - 55) / 20)
+        self.scale = int((self._height - 20) / 10)
         self.we_are_sharing = False
 
         self.move_list = []
